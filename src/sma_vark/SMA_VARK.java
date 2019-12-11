504da2fff9270e055f391389a957597fe4b94b83
Paul Ramon <paul98-18@hotmail.com>
2019-12-11 17:38:33 -0500

    Tercer comentario

--- src/sma_vark/SMA_VARK.java
+++ src/sma_vark/SMA_VARK.java
@@ -16,7 +16,7 @@ public class SMA_VARK {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Estoy enfermo");
+        System.out.println("Estoy enfermo fernando canta feo");
     }
     
 }
